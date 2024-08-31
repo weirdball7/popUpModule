369b16da61b9797e66cf6b281e382c96dc91998f
weirdball7 <moteamitay04@gmail.com>
2024-08-31 18:25:30 +0300

    created a pop up module, you just call the popUp function and enter the links you want as paramaters

---
+++ popUp.js
@@ -0,0 +1,21 @@
+export const popUp = (...links) => {
+    let existingPopUp = document.querySelector('.popup');
+    if (existingPopUp) {
+        existingPopUp.remove();
+        return;
+    }
+
+    const popUp = document.createElement('div');
+    popUp.classList.add('popup');
+    const linksList = document.createElement('ul');
+    links.forEach(link => {
+        const item = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.href = link;
+        anchor.textContent = link;
+        item.appendChild(anchor);
+        linksList.appendChild(item);
+    });
+    popUp.appendChild(linksList);
+    document.body.appendChild(popUp);
+};
